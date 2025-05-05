0021f3ecae48eab5553ddde219b945070bd33a28
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-05-05 21:29:15 +0200

    Fixed so that spring container can use backend_app database user.
    The issue was that the db-init.sql file had backend_app in both role and user, which caused an error with the wildcard hostname.

--- db-scripts/db-init.sql
+++ db-scripts/db-init.sql
@@ -2,10 +2,10 @@ DROP SCHEMA IF EXISTS `backend_db`;
 CREATE SCHEMA IF NOT EXISTS `backend_db`;
 USE `backend_db`;
 
-DROP USER IF EXISTS 'backend_app'@'localhost';
-DROP ROLE IF EXISTS  'backend_app';
-CREATE ROLE 'backend_app';
-GRANT ALL ON `backend_db`.* TO 'backend_app';
-CREATE USER 'backend_app'@'localhost' IDENTIFIED BY 'test1234';
-GRANT 'backend_app' TO 'backend_app'@'localhost';
-SET DEFAULT ROLE ALL TO 'backend_app'@'localhost';
+DROP USER IF EXISTS 'backend_app'@'%';
+DROP ROLE IF EXISTS  'backend_app_role';
+CREATE ROLE 'backend_app_role';
+GRANT ALL ON `backend_db`.* TO 'backend_app_role';
+CREATE USER 'backend_app'@'%' IDENTIFIED BY 'test1234';
+GRANT 'backend_app_role' TO 'backend_app'@'%';
+SET DEFAULT ROLE ALL TO 'backend_app'@'%';
