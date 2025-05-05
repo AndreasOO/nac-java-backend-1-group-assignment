cedeb0369c3ab7f735e8f27d7ccbb8edf3a9cfbf
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-05-05 13:23:42 +0200

    bugfix - adding db user init

--- db-scripts/db-init.sql
+++ db-scripts/db-init.sql
@@ -1,3 +1,11 @@
 DROP SCHEMA IF EXISTS `backend_db`;
 CREATE SCHEMA IF NOT EXISTS `backend_db`;
 USE `backend_db`;
+
+DROP USER IF EXISTS 'backend_app'@'localhost';
+DROP ROLE IF EXISTS  'backend_app';
+CREATE ROLE 'backend_app';
+GRANT ALL ON `backend_db`.* TO 'backend_app';
+CREATE USER 'backend_app'@'localhost' IDENTIFIED BY 'test1234';
+GRANT 'backend_app' TO 'backend_app'@'localhost';
+SET DEFAULT ROLE ALL TO 'backend_app'@'localhost';
