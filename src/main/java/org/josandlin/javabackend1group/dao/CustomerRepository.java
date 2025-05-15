fe33176aab124f84c7c744db06efccc61bf0b610
linn <linn.edvardsson@yh.nackademin.se>
2025-05-15 20:43:11 +0200

    customer-dao/repo

--- src/main/java/org/josandlin/javabackend1group/dao/CustomerRepository.java
+++ src/main/java/org/josandlin/javabackend1group/dao/CustomerRepository.java
@@ -8,12 +8,26 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
-import java.util.List;
 
 
 
 public interface CustomerRepository extends JpaRepository <Customer, Long> {
+    Customer findByName(String name);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Customer c " +
+            "WHERE c.id = :id " +
+            "AND NOT EXISTS (" +
+            "   SELECT b FROM Booking b WHERE b.customer.id = c.id" +
+            ")")
+    void deleteCustomerIfNoBookings(@Param("id") Long id);
+
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Customer c SET c.name = :newName WHERE c.id = :id")
+    void updateCustomer(@Param("newName") String newName, @Param("id") Long id);
 
 
 }
