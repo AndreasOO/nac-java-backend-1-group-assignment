de89c4fa5679c090eb71caf7a3c20ddbcda9f0da
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-05-17 15:02:53 +0200

    Added customer service methods

--- src/main/java/org/josandlin/javabackend1group/service/CustomerService.java
+++ src/main/java/org/josandlin/javabackend1group/service/CustomerService.java
@@ -1,6 +1,7 @@
 package org.josandlin.javabackend1group.service;
 
 import org.josandlin.javabackend1group.entity.BookedObject;
+import org.josandlin.javabackend1group.entity.Customer;
 import org.josandlin.javabackend1group.entity.Room;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,5 +10,16 @@ import java.util.List;
 
 
 public interface CustomerService {
+
+    @Transactional
+    Customer registerCustomer(Customer customer);
+
+    @Transactional
+    Customer editCustomer(Customer customer);
+
+    @Transactional
+    void deleteCustomer(Customer customer);
+
+
 
 }
--- src/main/java/org/josandlin/javabackend1group/service/CustomerServiceImpl.java
+++ src/main/java/org/josandlin/javabackend1group/service/CustomerServiceImpl.java
@@ -1,7 +1,47 @@
 package org.josandlin.javabackend1group.service;
 
+import org.josandlin.javabackend1group.dao.BookingDao;
+import org.josandlin.javabackend1group.dao.CustomerDao;
+import org.josandlin.javabackend1group.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomerServiceImpl {
+public class CustomerServiceImpl implements CustomerService {
+
+    private final BookingDao bookingDao;
+    private final CustomerDao customerDao;
+
+
+
+    @Autowired
+    public CustomerServiceImpl(BookingDao bookingDao,
+                               CustomerDao customerDao) {
+        this.bookingDao = bookingDao;
+        this.customerDao = customerDao;
+    }
+
+
+
+    @Override
+    public Customer registerCustomer(Customer customer) {
+        return customerDao.save(customer);
+    }
+
+    @Override
+    public Customer editCustomer(Customer customer) {
+        return customerDao.save(customer);
+    }
+
+    @Override
+    public void deleteCustomer(Customer customer) {
+        boolean hasBookings = bookingDao.findAll().stream().anyMatch(booking -> booking.getCustomer().getId().equals(customer.getId()));
+        if (hasBookings) {
+            //TODO add Optional.fail?
+            throw new IllegalArgumentException("Customer has active bookings, cannot delete");
+        } else {
+            customerDao.delete(customer);
+        }
+
+    }
 }
