fb13a5e1648b49e780379a50bc81ddec71208d67
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-05-19 22:36:11 +0200

    Setup boilerplate for integrationtests.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+DROP SCHEMA IF EXISTS `backend_db`;
+CREATE SCHEMA IF NOT EXISTS `backend_db`;
+USE `backend_db`;
--- src/test/java/org/josandlin/javabackend1group/JavaBackend1GroupApplicationTests.java
+++ src/test/java/org/josandlin/javabackend1group/JavaBackend1GroupApplicationTests.java
@@ -1,13 +1,86 @@
 package org.josandlin.javabackend1group;
 
+import org.josandlin.javabackend1group.entity.Booking;
+import org.josandlin.javabackend1group.entity.Customer;
+import org.josandlin.javabackend1group.service.BookingService;
+import org.josandlin.javabackend1group.service.CustomerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.List;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class JavaBackend1GroupApplicationTests {
+
+    @LocalServerPort
+    private Integer port;
+
+
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.4.4")
+            .withDatabaseName("backend_db")
+            .withUsername("backend_db")
+            .withPassword("test1234");
+            ;
+
+    @BeforeAll
+    static void beforeAll() {
+        mysql.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mysql.stop();
+    }
+
+//    @DynamicPropertySource
+//    static void configureProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+//        registry.add("spring.datasource.username", mysql::getUsername);
+//        registry.add("spring.datasource.password", mysql::getPassword);
+//    }
+
+
+    @Autowired
+    BookingService bookingService;
+
+    @Autowired
+    CustomerService customerService;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost:" + port;
+    }
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void shouldCreateBooking() {
+        Customer customer = new Customer(1L,"Test1");
+        Booking booking = new Booking(2L, customer);
+
+//        when(customerService.registerCustomer(customer)).thenReturn(customer);
+
+        Customer registeredCustomer = customerService.registerCustomer(customer);
+
+        assertThat(registeredCustomer.getId()).isEqualTo(1L);
+
     }
 
 }
