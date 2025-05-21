a1601bb36f450a2cf5bf98ea1185faba3dcc6d2e
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-05-21 14:31:33 +0200

    Added tests for customer edit and rooms

--- src/test/java/org/josandlin/javabackend1group/JavaBackend1GroupApplicationTests.java
+++ src/test/java/org/josandlin/javabackend1group/JavaBackend1GroupApplicationTests.java
@@ -1,14 +1,17 @@
 package org.josandlin.javabackend1group;
 
 import org.josandlin.javabackend1group.dao.*;
-import org.josandlin.javabackend1group.entity.Booking;
-import org.josandlin.javabackend1group.entity.Customer;
+import org.josandlin.javabackend1group.dto.BookingDTO;
+import org.josandlin.javabackend1group.dto.RoomDTO;
+import org.josandlin.javabackend1group.entity.*;
 import org.josandlin.javabackend1group.service.BookingService;
 import org.josandlin.javabackend1group.service.CustomerService;
+import org.josandlin.javabackend1group.service.RoomService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import io.restassured.RestAssured;
@@ -66,6 +69,9 @@ class JavaBackend1GroupApplicationTests {
     CustomerService customerService;
 
     @Autowired
+    RoomService roomService;
+
+    @Autowired
     CustomerDao customerDao;
 
     @Autowired
@@ -105,6 +111,7 @@ class JavaBackend1GroupApplicationTests {
     }
 
     @Test
+    //TODO change to DTO pattern
     void shouldRegisterCustomer() {
         //given
         Customer customer1 = new Customer("Test1");
@@ -121,6 +128,121 @@ class JavaBackend1GroupApplicationTests {
         assertThat(registeredCustomer2.getId()).isEqualTo(customer2.getId());
         assertThat(registeredCustomer3.getId()).isEqualTo(customer3.getId());
         assertThat(customerService.getAllCustomers().size()).isEqualTo(3);
+    }
+
+    @Test
+        //TODO change to DTO pattern
+    void shouldEditCustomer() {
+        //given
+        Customer customerOriginal = new Customer("TestOriginal");
+        Customer registeredCustomer = customerService.registerCustomer(customerOriginal);
+
+
+
+        //when
+        Customer customerWithEdits = new Customer(registeredCustomer.getId(),"TestAfterEdit");
+        Customer customerAfterEdits = customerService.editCustomer(customerWithEdits);
+
+
+        //then
+        assertThat(registeredCustomer.getId()).isEqualTo(customerAfterEdits.getId());
+        assertThat(registeredCustomer.getName()).isNotEqualTo(customerAfterEdits.getName());
+
+        assertThat(registeredCustomer.getName()).isEqualTo("TestOriginal");
+        assertThat(customerAfterEdits.getName()).isEqualTo("TestAfterEdit");
+
+    }
+
+    @Test
+        //TODO change to DTO pattern
+    void shouldDeleteCustomer() {
+        //given
+        Customer customerToBeDeleted = new Customer("customerToBeDeleted");
+        Customer registeredCustomerToBeDeleted = customerService.registerCustomer(customerToBeDeleted);
+
+        Customer customerToBeKept = new Customer("customerToBeKept");
+        Customer registeredCustomerToBeKept = customerService.registerCustomer(customerToBeKept);
+
+
+        //when
+        customerService.deleteCustomer(registeredCustomerToBeDeleted);
+
+
+        //then
+        assertThat(customerService.getAllCustomers().size()).isEqualTo(1);
+        assertThat(customerService.getAllCustomers().get(0).getId()).isEqualTo(registeredCustomerToBeKept.getId());
+        assertThat(customerService.getAllCustomers().get(0).getName()).isEqualTo("customerToBeKept");
+    }
+
+    @Test
+    void shouldNotDeleteCustomer() {
+        //given
+        Customer customerToTryBeDeleted = new Customer("customerToTryBeDeleted");
+        Customer registeredCustomerToTryBeDeleted = customerService.registerCustomer(customerToTryBeDeleted);
+        BookingDTO booking = bookingService.createBooking(registeredCustomerToTryBeDeleted.getId());
+
+
+        Customer customerToBeKept = new Customer("customerToBeKept");
+        Customer registeredCustomerToBeKept = customerService.registerCustomer(customerToBeKept);
+
+
+        //when
+
+        //then
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> customerService.deleteCustomer(registeredCustomerToTryBeDeleted));
+        assertThat(exception.getMessage().equals("Customer has active bookings, cannot delete")).isTrue();
+        assertThat(customerService.getAllCustomers().size()).isEqualTo(2);
+    }
+
+    @Test
+    void shouldHaveRoomsWithExtraBeds() {
+        //given
+        RoomType singleRoom1 = new RoomType("Single room", 1000);
+        RoomType doubleRoom = new RoomType("Double room", 2000);
+        RoomType twinRoom = new RoomType("Twin room", 1900);
+        RoomType singleRoom2 = new RoomType("Single room", 3200);
+
+        roomTypeDao.save(singleRoom1);
+        roomTypeDao.save(doubleRoom);
+        roomTypeDao.save(twinRoom);
+        roomTypeDao.save(singleRoom2);
+
+
+        Room roomOne = new Room("Sea view room", 3, 1, doubleRoom); // millys bokning + andreas bokning (lagt till en extra bed)
+        Room roomTwo = new Room("Dumpster room", 2, 0, twinRoom); // olas bokning
+        Room roomThree = new Room("Honeymoon suite", 4, 2, doubleRoom); // linns bokning
+        Room roomFour = new Room("Nice room", 1, 0, singleRoom1); // josefins bokning
+        Room roomFive = new Room("Ok room", 1, 0, singleRoom2); //olas bokning
+        Room roomSix = new Room("Unbooked room", 1, 1, singleRoom1);
+
+        roomDao.save(roomOne);
+        roomDao.save(roomTwo);
+        roomDao.save(roomThree);
+        roomDao.save(roomFour);
+        roomDao.save(roomFive);
+        roomDao.save(roomSix);
+
+        //when
+        List<RoomDTO> allRooms = roomService.getAllRooms();
+
+        //then
+        assertThat(allRooms).hasSize(6);
+        assertThat(allRooms.stream().filter(room -> room.getName().equals("Sea view room")).findFirst().get().getExtraBedsAvailable())
+                                                                                                                      .isEqualTo(1);
+        assertThat(allRooms.stream().filter(room -> room.getName().equals("Dumpster room")).findFirst().get().getExtraBedsAvailable())
+                                                                                                                      .isEqualTo(0);
+        assertThat(allRooms.stream().filter(room -> room.getName().equals("Honeymoon suite")).findFirst().get().getExtraBedsAvailable())
+                .isEqualTo(2);
+
+        assertThat(allRooms.stream().filter(room -> room.getName().equals("Nice room")).findFirst().get().getExtraBedsAvailable())
+                .isEqualTo(0);
+
+        //
+        assertThat(allRooms.stream().filter(room -> room.getName().equals("Ok room")).findFirst().get().getExtraBedsAvailable())
+                .isEqualTo(0);
+        assertThat(allRooms.stream().filter(room -> room.getName().equals("Unbooked room")).findFirst().get().getExtraBedsAvailable())
+                .isEqualTo(1);
+
     }
 
 }
